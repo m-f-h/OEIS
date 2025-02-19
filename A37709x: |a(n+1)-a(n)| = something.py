b000c9aaba3c84f0bda1a7af7f99a36200d08979
m-f-h <github@dsi972.fr>
2025-02-19 12:12:55 -0400

    Update A37709x: |a(n+1)-a(n)| = something.py

--- A37709x: |a(n+1)-a(n)| = something.py
+++ A37709x: |a(n+1)-a(n)| = something.py
@@ -7,32 +7,7 @@ A377092: something = Fibonacci
 
 See also A377090 for a simpler variant
 '''
-
 from sympy import isprime # for A377092
-
-# version 0: the simplest would be to use "optional keyword arguments" for static variables
-def A377090(n, terms = [0]):    # A377090.N = least unused candidate
-    while len(terms) <= n:
-        while (k := getattr(A377090,'N',1)) in terms: A377090.N = (k<0)-k
-        while not isprime(abs(k - terms[-1])) or k in terms: k = (k<0)-k
-        terms.append(k)
-    return terms[n] # _M. F. Hasler_, Feb 10 2025
-
-# But this becomes ugly when 'terms' becomes very large, because most Python
-# interfaces show the value of 'terms' when a user types " A377090( ".
-
-# version 1
-def A377090(n):    # A377090.N = least unused candidate
-    if not getattr(A := A377090, 'N', 0):  A.N = 1; A.terms = [0]
-    while len(A.terms) <= n:
-        while (k := A.N) in A.terms: A.N = (k<0)-k
-        while not isprime(abs(k - A.terms[-1])) or k in A.terms: k = (k<0)-k
-        A.terms.append(k)
-    return A.terms[n] # _M. F. Hasler_, Feb 10 2025
-
-# it may be considered elegant to "hide" all technicalities inside the function,
-# but this way there is the "getattr" call at each call of the function...
-# maybe better "hard-wire" this? see next version.
 
 # version 1-b
 def A377090(n = None, ok = isprime):
@@ -44,42 +19,24 @@ def A377090(n = None, ok = isprime):
         while not ok(abs(k - A.terms[-1])) or k in A.terms: k = (k<0)-k
         A.terms.append(k)
     return A.terms[n] if n or n==0 else(A(n,ok) for n in range(1<<63))# "infinite"
-  # _M. F. Hasler_, Feb 10 2025
-   
+    # Possible extension: optional 'stop' parameter, then use 
+    # (... for n in range(n or 0, stop or 2**63))
+# _M. F. Hasler_, Feb 10 2025
+
 # Now we could implement additional functionality:
 # for example:
 # * A377090() - generator of the entire (infinite) sequence
 # * A377090(n) - return the n-th element
 # * A377090(start, stop [, step]) - return [generator or sequence?] of sequence from start to stop [by step ?]
 
-# version 3
-def A377090(n = None, stop = None):
-    if n is None or stop is not None: return(A377090(n) for n in range(n or 0, stop or 2**63))
-    while len(terms := A377090.terms) <= n:
-        while (k := A377090.N) in terms: A377090.N = (k<0)-k
-        while not isprime(abs(k - terms[-1])) or k in terms: k = (k<0)-k
-        terms.append(k)
-    return terms[n] # _M. F. Hasler_, Feb 10 2025
-A377090.terms = [0]; A377090.N = 1 # least unused candidate.  # initialization.
-
-
-# version 2. The class structure is mainly used to hide the terms and 'N' inside 
-class A377090: # A377090(n) gives a(n)
-    def __new__(A, n):  # or use __class_getitem__ to allow slices A377090[a:b:c]
-        while len(A.terms) <= n: # if type(n)==int else max(n.start or 0, ...)
-            while (k := A.N) in A.terms: A.N = (k<0)-k
-            while not isprime(abs(k-A.terms[-1])) or k in A.terms: k = (k<0)-k
-            A.terms.append(k)
-        return A.terms[n] # _M. F. Hasler_, Feb 10 2025
-    terms = [0]; N = 1 # least unused candidate
-
 # version 2-b: use __class_getitem__ to allow slices A377090[a:b:c]
-class A37709x:
+class A377090:
     """A37709x(n) and A37709x[n] gives a(n); A377090[slice] gives a slice (a(k); k in range(*slice.indices)).
     Uses the class method/function ok(): set this to whatever you want (isprime, issquare, is_Axxx, ...)
     for the specific sequence (A377090, A377091, A377092, ...)
     """
-    def __class_getitem__(A, n = None): #This part can be omitted 
+    ok = isprime  # default: A377090
+    def __class_getitem__(A, n = None): # This part can be omitted 
         if type(n)==int: return A(n)
         if n is None: return(A(n)for n in range(1<<63)) # quasi-infinite generator
         if type(n)==type: return ... # GenericAlias
@@ -94,5 +51,9 @@ class A37709x:
             A.terms.append(k)
         return A.terms[n] # _M. F. Hasler_, Feb 10 2025
     terms = [0]; N = 1 # least unused candidate
+class A377091(A377090): # note: it is essential to initialize N and terms in each subclass!
+    terms=[0]; N=1; ok = lambda n: (n**.5).is_integer() # issquare
+class A377090(A377090):
+    terms=[0]; N=1; ok = is_A000045 # is_fibonacci
 
 
